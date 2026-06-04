26ea89d4571c64860761518c5ffc494e82bfd454
bwgabor <gabor.sziebert@gmail.com>
2026-06-04 10:22:48 +0200

    feat: add custom Jenkins image with Docker CLI and Maven

---
+++ docker-files/jenkins/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# File: docker-files/jenkins/entrypoint.sh
+# Purpose: Align docker group GID at container startup
+DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+groupmod -g "$DOCKER_GID" docker 2>/dev/null || groupadd -g "$DOCKER_GID" docker
+usermod -aG docker jenkins
+exec /usr/bin/tini -- /usr/local/bin/jenkins.sh
