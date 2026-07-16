21d64ddd1b82a7797ca6eaef23d05140efd28d6b
bwgabor <gabor.sziebert@gmail.com>
2026-07-16 16:58:40 +0200

    build: add custom Jenkins image with Docker socket access

--- docker-files/entrypoint.sh
+++ docker-files/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# File: docker-files/jenkins/entrypoint.sh
+# File: docker-files/entrypoint.sh
 # Purpose: Align docker group GID at container startup
+
+set -e
+
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 groupmod -g "$DOCKER_GID" docker 2>/dev/null || groupadd -g "$DOCKER_GID" docker
 usermod -aG docker jenkins
-exec /usr/bin/tini -- /usr/local/bin/jenkins.sh
+
+exec su -s /bin/bash jenkins -c "/usr/bin/tini -- /usr/local/bin/jenkins.sh"
